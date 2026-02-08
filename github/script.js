7ef27728fa3217f0a667a0656505efa05676d325
MR. Anonymous <marufhassan.rudro121@gmail.com>
2026-02-08 21:59:46 +0600

    Delete github directory

--- github/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const name = document.querySelector('input[type="text"]').value;
-    
-    // Simple logic to show it's working
-    console.log("Logged in by dev_maruf");
-    alert("Jutsu Activated!🐦‍🔥 Welcome, Shinobi " + name);
-});
